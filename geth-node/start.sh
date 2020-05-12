d33573ae2375d9f8564e906bb0b5952b27f3796a
Xinlei Ren <1632553727@qq.com>
2020-05-12 01:09:45 -0700

    bug

--- geth-node/start.sh
+++ geth-node/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 cd /root/eth-net-intelligence-api
-perl -pi -e "s/XXX/$(HOSTNAME)/g" app.json
+perl -pi -e "s/XXX/renxinlei/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
 geth --datadir=~/.ethereum/devchain init "/root/genesis/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
-GETH_OPTS=${@/$(HOSTNAME)/$BOOTSTRAP_IP}
+GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
 geth $GETH_OPTS
