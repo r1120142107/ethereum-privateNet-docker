88410e9118d2cab04971a64953d4ab8a365ad0a9
Xinlei Ren <1632553727@qq.com>
2020-05-16 20:06:47 -0700

    bug solution

--- geth-node/start.sh
+++ geth-node/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd /root/eth-net-intelligence-api
-perl -pi -e "s/XXX/renxinlei/g" app.json
+perl -pi -e "s/XXX/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
 geth --datadir=~/.ethereum/devchain init "/root/genesis/genesis.json"
